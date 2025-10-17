93344b89781d87ca7cf3528e28fffc2bb389152e
Viiiin94 <mynameisyyb@naver.com>
2025-10-17 14:38:54 +0900

    update stm32 settings

---
+++ Core/Inc/main.h
@@ -0,0 +1,123 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define STEP_OUTPUT0_Pin GPIO_PIN_0
+#define STEP_OUTPUT0_GPIO_Port GPIOA
+#define STEP_OUTPUT1_Pin GPIO_PIN_1
+#define STEP_OUTPUT1_GPIO_Port GPIOA
+#define STEP_OUTPUT2_Pin GPIO_PIN_4
+#define STEP_OUTPUT2_GPIO_Port GPIOA
+#define LED_OUTPUT0_Pin GPIO_PIN_5
+#define LED_OUTPUT0_GPIO_Port GPIOA
+#define LED_OUTPUT1_Pin GPIO_PIN_6
+#define LED_OUTPUT1_GPIO_Port GPIOA
+#define LED_OUTPUT2_Pin GPIO_PIN_7
+#define LED_OUTPUT2_GPIO_Port GPIOA
+#define EXIT_2F_Pin GPIO_PIN_5
+#define EXIT_2F_GPIO_Port GPIOC
+#define STEP_OUTPUT3_Pin GPIO_PIN_0
+#define STEP_OUTPUT3_GPIO_Port GPIOB
+#define FND_OUTPUT4_Pin GPIO_PIN_1
+#define FND_OUTPUT4_GPIO_Port GPIOB
+#define FND_OUTPUT3_Pin GPIO_PIN_2
+#define FND_OUTPUT3_GPIO_Port GPIOB
+#define LED_OUTPUT7_Pin GPIO_PIN_10
+#define LED_OUTPUT7_GPIO_Port GPIOB
+#define FND_OUTPUT2_Pin GPIO_PIN_12
+#define FND_OUTPUT2_GPIO_Port GPIOB
+#define FND_OUTPUT7_Pin GPIO_PIN_13
+#define FND_OUTPUT7_GPIO_Port GPIOB
+#define FND_OUTPUT6_Pin GPIO_PIN_14
+#define FND_OUTPUT6_GPIO_Port GPIOB
+#define FND_OUTPUT5_Pin GPIO_PIN_15
+#define FND_OUTPUT5_GPIO_Port GPIOB
+#define EXIT_1F_Pin GPIO_PIN_6
+#define EXIT_1F_GPIO_Port GPIOC
+#define LED_OUTPUT4_Pin GPIO_PIN_7
+#define LED_OUTPUT4_GPIO_Port GPIOC
+#define EXIT_0F_Pin GPIO_PIN_8
+#define EXIT_0F_GPIO_Port GPIOC
+#define LED_OUTPUT6_Pin GPIO_PIN_8
+#define LED_OUTPUT6_GPIO_Port GPIOA
+#define LED_OUTPUT5_Pin GPIO_PIN_9
+#define LED_OUTPUT5_GPIO_Port GPIOA
+#define FND_OUTPUT1_Pin GPIO_PIN_11
+#define FND_OUTPUT1_GPIO_Port GPIOA
+#define FND_OUTPUT0_Pin GPIO_PIN_12
+#define FND_OUTPUT0_GPIO_Port GPIOA
+#define BUZZER_Pin GPIO_PIN_15
+#define BUZZER_GPIO_Port GPIOA
+#define BUTTON_INPUT2_Pin GPIO_PIN_3
+#define BUTTON_INPUT2_GPIO_Port GPIOB
+#define BUTTON_INPUT0_Pin GPIO_PIN_4
+#define BUTTON_INPUT0_GPIO_Port GPIOB
+#define BUTTON_INPUT1_Pin GPIO_PIN_5
+#define BUTTON_INPUT1_GPIO_Port GPIOB
+#define LED_OUTPUT3_Pin GPIO_PIN_6
+#define LED_OUTPUT3_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/gpio.c
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, STEP_OUTPUT0_Pin|STEP_OUTPUT1_Pin|STEP_OUTPUT2_Pin|LED_OUTPUT0_Pin
+                          |LED_OUTPUT1_Pin|LED_OUTPUT2_Pin|LED_OUTPUT6_Pin|LED_OUTPUT5_Pin
+                          |FND_OUTPUT1_Pin|FND_OUTPUT0_Pin|BUZZER_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, STEP_OUTPUT3_Pin|FND_OUTPUT4_Pin|FND_OUTPUT3_Pin|LED_OUTPUT7_Pin
+                          |FND_OUTPUT2_Pin|FND_OUTPUT7_Pin|FND_OUTPUT6_Pin|FND_OUTPUT5_Pin
+                          |LED_OUTPUT3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_OUTPUT4_GPIO_Port, LED_OUTPUT4_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : STEP_OUTPUT0_Pin STEP_OUTPUT1_Pin STEP_OUTPUT2_Pin LED_OUTPUT0_Pin
+                           LED_OUTPUT1_Pin LED_OUTPUT2_Pin LED_OUTPUT6_Pin LED_OUTPUT5_Pin
+                           FND_OUTPUT1_Pin FND_OUTPUT0_Pin BUZZER_Pin */
+  GPIO_InitStruct.Pin = STEP_OUTPUT0_Pin|STEP_OUTPUT1_Pin|STEP_OUTPUT2_Pin|LED_OUTPUT0_Pin
+                          |LED_OUTPUT1_Pin|LED_OUTPUT2_Pin|LED_OUTPUT6_Pin|LED_OUTPUT5_Pin
+                          |FND_OUTPUT1_Pin|FND_OUTPUT0_Pin|BUZZER_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : EXIT_2F_Pin EXIT_1F_Pin EXIT_0F_Pin */
+  GPIO_InitStruct.Pin = EXIT_2F_Pin|EXIT_1F_Pin|EXIT_0F_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : STEP_OUTPUT3_Pin FND_OUTPUT4_Pin FND_OUTPUT3_Pin LED_OUTPUT7_Pin
+                           FND_OUTPUT2_Pin FND_OUTPUT7_Pin FND_OUTPUT6_Pin FND_OUTPUT5_Pin
+                           LED_OUTPUT3_Pin */
+  GPIO_InitStruct.Pin = STEP_OUTPUT3_Pin|FND_OUTPUT4_Pin|FND_OUTPUT3_Pin|LED_OUTPUT7_Pin
+                          |FND_OUTPUT2_Pin|FND_OUTPUT7_Pin|FND_OUTPUT6_Pin|FND_OUTPUT5_Pin
+                          |LED_OUTPUT3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED_OUTPUT4_Pin */
+  GPIO_InitStruct.Pin = LED_OUTPUT4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_OUTPUT4_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BUTTON_INPUT2_Pin BUTTON_INPUT0_Pin BUTTON_INPUT1_Pin */
+  GPIO_InitStruct.Pin = BUTTON_INPUT2_Pin|BUTTON_INPUT0_Pin|BUTTON_INPUT1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
